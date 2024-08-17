f1b4885cf31a75bc3660137e4e3429fe98ea56f0
Kharlamov Egor <kharlamovegormain@mail.ru>
2024-08-17 20:55:56 +0400

    faq is added

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const faqItems = document.querySelectorAll('.faq__grid-item');
+
+faqItems.forEach((faqItem) => {
+  faqItem.children[0].addEventListener('click', () => {
+    faqItem.classList.toggle('open');
+  });
+});
